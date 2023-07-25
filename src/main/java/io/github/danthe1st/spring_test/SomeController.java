4b07ec944a0b4259f7485f880b9f902ce47d08cd
danthe1st <daniel@wwwmaster.at>
2023-07-25 13:44:49 +0200

    add hibernate enhancer, remove MultipartFile and transactional

--- src/main/java/io/github/danthe1st/spring_test/SomeController.java
+++ src/main/java/io/github/danthe1st/spring_test/SomeController.java
@@ -1,20 +1,16 @@
 package io.github.danthe1st.spring_test;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class SomeController {
 
 	@PostMapping
-	public void someEndpoint(@RequestParam(name = "a") MultipartFile imageFile) {
+	public void someEndpoint() {
 		somethingTransactional();
 	}
 
-	@Transactional
 	public void somethingTransactional() {
 
 	}
